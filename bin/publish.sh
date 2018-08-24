fe51759cf9e5478161798fa28f27a56edf569647
Jack Reed <phillipjreed@gmail.com>
2018-08-24 12:12:32 -0600

    add initial deploy script

---
+++ bin/publish.sh
@@ -0,0 +1,19 @@
+npm i
+
+npm run build
+
+# checkout to the gh-pages branch
+git checkout gh-pages
+
+# pull the latest updates
+git pull origin gh-pages --rebase
+
+cp dist/* ./
+
+git add index.html bundle.js
+
+git commit -m 'Update sites'
+
+git push origin gh-pages
+
+git checkout master
