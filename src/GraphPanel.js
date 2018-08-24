d446dc371e671341b597468863bd79d0722ee100
Andrew Winget (Standard) <scipioaffricanus@gmail.com>
2018-08-24 16:32:58 -0700

    update graph panel

--- src/GraphPanel.js
+++ src/GraphPanel.js
@@ -1,9 +1,14 @@
 import React from 'react'
 
 const GraphPanel = props => (
-    <div>
-    <h3>Graphs</h3>
-    <iframe src="https://rpm.newrelic.com/public/charts/9kReJWHRXyt" width="500" height="300" scrolling="no" frameBorder="no"></iframe>
+    <div className="section">
+    <h1>Graphs</h1>
+    <h2>Searchworks Page Load Time</h2>
+    <iframe src="https://rpm.newrelic.com/public/charts/9kReJWHRXyt" width="100%" height="300" scrolling="no" frameBorder="no"></iframe>
+    <h2>SOLR Response Time</h2>
+    <iframe src="https://rpm.newrelic.com/public/charts/tAPstSMLa7" width="100%" height="300" scrolling="no" frameborder="no"></iframe>
+    <h2>Ebsco Response Time</h2>
+  <iframe src="https://rpm.newrelic.com/public/charts/gaCwQOqamfZ" width="100%" height="300" scrolling="no" frameborder="no"></iframe>
     </div>
 )
 
