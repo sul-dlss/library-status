622c9ef68f3db42ce568e87087409db5387d7beb
Andrew Winget (Standard) <scipioaffricanus@gmail.com>
2018-08-27 16:03:11 -0700

    clean up header styling

--- src/Header.js
+++ src/Header.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Header = props => (
     <div id="header">
       <div className="title">
-        <span>Searchworks Status</span>
+        <h1>Searchworks Status</h1>
       </div>
       <div className="nav-menu">
         <a href="/">Dashboard</a>
