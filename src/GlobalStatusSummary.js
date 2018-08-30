f03bb9f1ba62f8c85e50d4f38a30db63f6beae36
Andrew Winget (Standard) <scipioaffricanus@gmail.com>
2018-08-30 13:47:06 -0700

    add global status check

--- src/GlobalStatusSummary.js
+++ src/GlobalStatusSummary.js
@@ -1,10 +1,25 @@
 import React from 'react'
 
-class GlobalStatusSummary extends React.Component {
-  render() {
-    return <div id="GlobalStatusSummary">
-      </div>
-  }
+const statusReducer = (endpointStatuses) => {
+  // If all statuses are green, we're green
+
+  // If we're still waiting on some statuses,
+  // we're pending.
+
+  // Searchworks is up, but some items are experiencing
+  // issues, we're sort of up.
+
+  // If we're in the maintenance window, we are
+  // under maintenance.
+  return 'pending'
 }
+
+const GlobalStatusSummary = props => (
+   <div id="GlobalStatusSummary">
+     <h1>{props.statuses[statusReducer(props.endpoints)].icon}</h1>
+     <h3>Retrieving status information</h3>
+     <p>This will only take a moment.</p>
+   </div>
+)
 
 export default GlobalStatusSummary
