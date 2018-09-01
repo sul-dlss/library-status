f5502f9217182e2a535e37c6f3d0de8344f1158d
Jessie Keck <jessie.keck@gmail.com>
2018-08-31 17:37:20 -0700

    Add an anchor and update header links to take user to that anchor.

--- src/GraphPanel.js
+++ src/GraphPanel.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const GraphPanel = props => (
     <div className="section">
-    <h1>Graphs</h1>
+    <h1 id="graphs">Graphs</h1>
     <h2>Searchworks Page Load Time</h2>
     <iframe src="https://rpm.newrelic.com/public/charts/9kReJWHRXyt" width="100%" height="300" scrolling="no" frameBorder="no"></iframe>
     <h2>SOLR Response Time</h2>
