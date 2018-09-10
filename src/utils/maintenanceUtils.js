6f03ba2cf4eefe0fa48a2e524a0ce2427accf490
Jack Reed <phillipjreed@gmail.com>
2018-09-10 12:09:15 -0600

    add a test for our our maintenance window calculator, and use DI

--- src/utils/maintenanceUtils.js
+++ src/utils/maintenanceUtils.js
@@ -1,21 +1,21 @@
 import moment from 'moment'
 import momentTimezone from 'moment-timezone'
-function isBeingMaintained (interval) {
+function isBeingMaintained (currentTime, interval) {
   // Given a maintenance interval, return whether
   // the current time is within that interval.
   // Take timezone into account.
   // let currentDate = new Date
-  let currentDate = moment(new Date).tz('America/Los_Angeles')
+  let currentDate = moment(currentTime).tz('America/Los_Angeles')
   let currentHour = currentDate.hour()
   let maintenanceDay = (interval.day === currentDate.day())
   let maintenanceInterval = (currentHour >= interval.startHour) && (currentHour < interval.endHour)
   return (maintenanceDay && maintenanceInterval)
 }
 
-export default function areBeingMaintained(intervals){
+export default function areBeingMaintained(currentTime, intervals){
   let isDown = false
   intervals.forEach((interval) => {
-    if (isBeingMaintained(interval)) {
+    if (isBeingMaintained(currentTime, interval)) {
       isDown = true
     }
   })
