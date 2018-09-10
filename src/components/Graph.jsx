47a13d4ba98a24867a59c7dd9bd281c2c50870e7
Jack Reed <phillipjreed@gmail.com>
2018-09-10 17:20:24 -0600

    iframe must have a unique title

--- src/components/Graph.jsx
+++ src/components/Graph.jsx
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 const Graph = ({ graph }) => (
   <div>
     <h2>{ graph.title }</h2>
-    <iframe src={graph.iframeSrc} width="100%" height="300" scrolling="no" frameBorder="no" />
+    <iframe src={graph.iframeSrc} title={graph.title} width="100%" height="300" scrolling="no" frameBorder="no" />
   </div>
 );
 
