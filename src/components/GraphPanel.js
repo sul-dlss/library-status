db272005b08efb9b8192ff65628711a57c65eaa8
Jessie Keck <jessie.keck@gmail.com>
2018-09-10 12:38:36 -0700

    Use graphs as configured in the GraphPanel component (and add a new Graph component to represent a signe heading/graph).

--- src/components/GraphPanel.js
+++ src/components/GraphPanel.js
@@ -1,15 +1,15 @@
 import React from 'react'
+import Graph from './Graph'
 
 const GraphPanel = props => (
-    <div className="section">
+  <div className="section">
     <h1 id="graphs">Graphs</h1>
-    <h2>Searchworks Page Load Time</h2>
-    <iframe src="https://rpm.newrelic.com/public/charts/5f3weQre01n" width="100%" height="300" scrolling="no" frameBorder="no"></iframe>
-    <h2>SOLR Response Time</h2>
-    <iframe src="https://rpm.newrelic.com/public/charts/iM3aQqmT3Yt" width="100%" height="300" scrolling="no" frameBorder="no"></iframe>
-    <h2>Ebsco Response Time</h2>
-  <iframe src="https://rpm.newrelic.com/public/charts/lFCu4RDWoE2" width="100%" height="300" scrolling="no" frameBorder="no"></iframe>
-    </div>
+    {
+      Object.keys(props.graphs).map((graphKey) => {
+        return(<Graph graph={props.graphs[graphKey]} />)
+      })
+    }
+  </div>
 )
 
 export default GraphPanel
