aedacbe7476e59e0fd09702fad081863679c1b69
Jack Reed <phillipjreed@gmail.com>
2018-09-27 16:53:33 -0600

    modify styling to enhance responsiveness

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import SulLogo from '../styles/sul-logo.svg';
+import SulRosette from '../styles/sul-rosette.svg';
 
 const Header = () => (
   <div id="header">
     <div className="brand-logo">
-      <img src={SulLogo} alt="" height="35px" />
+      <img src={SulLogo} alt="" height="35px" className="display-large" />
+      <img src={SulRosette} alt="" height="35px" className="display-small" />
     </div>
     <div className="title">
       <h1>SearchWorks status</h1>
--- src/components/StatusHeader.jsx
+++ src/components/StatusHeader.jsx
@@ -1,31 +1,9 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 
-const StatusHeader = ({ statuses }) => (
+const StatusHeader = () => (
   <div id="status-header">
     <h2>Current service status</h2>
-    <div className="status-legend">
-      {Object.keys(statuses)
-        .map((status) => {
-          const currentStatus = statuses[status];
-          if (status !== 'pending' && status !== 'fatal') {
-            return (
-              <div key={status} className="status-legend-item">
-                <p>
-                  <span>{currentStatus.icon}</span>
-                  {currentStatus.legend}
-                </p>
-              </div>
-            );
-          }
-          return '';
-        })}
-    </div>
   </div>
 );
-
-StatusHeader.propTypes = {
-  statuses: PropTypes.object.isRequired, // eslint-disable-line react/forbid-prop-types
-};
 
 export default StatusHeader;
