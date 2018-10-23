0107cd5cc97bf55cc170b5958740ea37ef18133c
Camille Villa <cvilla100@gmail.com>
2018-10-23 16:20:31 -0700

    Add feedback links to footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Footer = () => (
+  <div id="footer">
+    <p>
+      Need to reach us? Try
+      <a href="https://searchworks.stanford.edu/feedback">
+        SearchWorks feedback
+      </a>
+       or
+      <a href="https://library.stanford.edu/ask/email/feedback">
+        Library website feedback.
+      </a>
+    </p>
+  </div>
+);
+
+export default Footer;
